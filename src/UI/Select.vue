ad82fdf2c03dc047fe3d62d48e51118f83b8b971
Anflame <anflame@mail.ru>
2022-12-01 11:47:30 +0400

    fixed select

--- src/UI/Select.vue
+++ src/UI/Select.vue
@@ -1,6 +1,6 @@
 <template>
-  <select :model="modelValue" @change="changeValue">
-    <option disabled value="" selected>Выберите из списка</option>
+  <select :value="modelValue" @change="changeValue">
+    <option disabled value="">Выберите из списка</option>
     <option v-for="option in options" :value="option.value" :key="option.value">{{ option.name }}</option>
   </select>
 </template>
