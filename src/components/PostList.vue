6f40df2657cb6392cf3ec37b5e7de218486e0e87
Anflame <anflame@mail.ru>
2022-12-01 13:37:31 +0400

    post lists with pegianation

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,11 +1,14 @@
 <template>
   <div v-if="(posts.length > 0)">
     <h3>Список постов</h3>
-    <PostItem
-      v-for="post in posts"
-      :post="post"
-      @remove="$emit('remove', post)"
-    />
+    <transition-group name="post-list" appear>
+      <PostItem
+        v-for="post in posts"
+        :post="post"
+        :key="post.id"
+        @remove="$emit('remove', post)"
+      />
+    </transition-group>
   </div>
   <h2 v-else style="color: red; text-align: center;">Список пуст</h2>
 </template>
@@ -26,4 +29,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all .4s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+.post-list-move {
+  transition: transform .8s ease;
+}
 </style>
